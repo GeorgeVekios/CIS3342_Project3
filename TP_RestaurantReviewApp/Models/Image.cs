66d6524143fdbc8dc9604483b83f7a9b118ee669
georgev <georgevekios@gmail.com>
2025-04-02 14:55:40 -0400

    Added Image class into Models. Coded images

---
+++ TP_RestaurantReviewApp/Models/Image.cs
@@ -0,0 +1,35 @@
+﻿using system;
+
+public class image
+{
+    private string fileLocation;
+    private string caption;
+    private string storagesize;
+    private int imageId;
+
+    
+    //default constructor
+    public image()
+    {
+        fileLocation = string.Empty;
+        caption = string.Empty;
+        storagesize = string.Empty;
+        imageId = -1;
+        
+    }
+
+    //parameterized
+    public image(string FileLocation, string Caption, string StorageSize, int ImageId)
+    {
+        this.fileLocation = FileLocation;
+        this.caption = Caption;
+        this.storagesize = StorageSize;
+        this.imageId = ImageId;
+    }
+
+    //getters, setters
+    public string FileLocation { get { return fileLocation; } set { this.fileLocation = value; } }
+    public string Caption { get { return caption; } set { this.caption = value; } }
+    public string StorageSize { get { return StorageSize; } set { this.StorageSize = value; } }
+    public int ImageId { get { return imageId; } set { this.imageId = value; } }
+}
