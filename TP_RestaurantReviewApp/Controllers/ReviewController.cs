0486ce890344c09406a2a3d4fc89083e4bdae988
tuo77176 <tuo77176@temple.edu>
2025-04-07 16:43:18 -0700

    4/7/25 - Added 3 Class Libraries for Separation of Operations. Included in this commit is new Review Controller and Review Object.

---
+++ TP_RestaurantReviewApp/Controllers/ReviewController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TP_RestaurantReviewApp.Controllers
+{
+    public class ReviewController : Controller
+    {
+        [HttpPost()]
+        public IActionResult CreateReview()
+        {
+            return View();
+        }
+    }
+}
