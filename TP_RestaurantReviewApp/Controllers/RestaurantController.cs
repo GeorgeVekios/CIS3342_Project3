1ef1e58357a995a1d17f1aa24e9d8d76fd9db5eb
tuo77176 <tuo77176@temple.edu>
2025-04-07 13:20:55 -0700

    Updated Search Restaurants Functionality to accept the correct params, load Cuisines from the database, etc. Still some changes to be made.

--- TP_RestaurantReviewApp/Controllers/RestaurantController.cs
+++ TP_RestaurantReviewApp/Controllers/RestaurantController.cs
@@ -50,43 +50,113 @@ namespace TP_RestaurantReviewApp.Controllers
         }
 
         [HttpGet("Restaurant/SearchRestaurants")]
-        public IActionResult LoadAllRestaurants()
+        public IActionResult SearchRestaurants(string city, string state, List<string> cuisines)
         {
+            bool isSearch = !string.IsNullOrEmpty(city) || !string.IsNullOrEmpty(state) || (cuisines != null && cuisines.Any());
             List<Restaurant> restaurantList = new List<Restaurant>();
-            DBConnect dBConnect = new DBConnect();
+
+            if (isSearch)
+            {
+                string cuisineList = "";
+
+                if (cuisines != null)
+                {
+                    cuisineList = string.Join(",", cuisines);
+                }
+
+                DBConnect dBConnect = new DBConnect();
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "TP_SearchRestaurants";
+                cmd.Parameters.AddWithValue("@City", city);
+                cmd.Parameters.AddWithValue("@State", state);
+                cmd.Parameters.AddWithValue("@CuisineList", cuisineList);
+
+                DataSet ds = dBConnect.GetDataSetUsingCmdObj(cmd);
+
+                foreach (DataRow record in ds.Tables[0].Rows)
+                {
+                    Restaurant restaurant = new Restaurant();
+
+                    restaurant.RestaurantID = Convert.ToInt32(record["RestaurantID"]);
+                    restaurant.OwnerID = Convert.ToInt32(record["OwnerID"]);
+                    restaurant.Name = record["Name"].ToString();
+                    restaurant.Cuisine = record["Cuisine"].ToString();
+                    restaurant.StreetAddress = record["StreetAddress"].ToString();
+                    restaurant.City = record["City"].ToString();
+                    restaurant.State = record["State"].ToString();
+                    restaurant.ZipCode = Convert.ToInt32(record["ZipCode"]);
+                    restaurant.HoursOfOperation = record["HoursOfOperation"].ToString();
+                    restaurant.Email = record["Email"].ToString();
+                    restaurant.PhoneNum = record["PhoneNumber"].ToString();
+                    restaurant.Description = record["Description"].ToString();
+                    restaurant.OverallRating = Convert.ToDouble(record["OverallRating"]);
+                    restaurant.AvgFoodRating = Convert.ToDouble(record["AvgFoodRating"]);
+                    restaurant.AvgServiceRating = Convert.ToDouble(record["AvgServiceRating"]);
+                    restaurant.AvgAtmosphereRating = Convert.ToDouble(record["AvgAtmosphereRating"]);
+                    restaurant.AvgPriceRating = Convert.ToDouble(record["AvgPriceRating"]);
+                    restaurant.WebsiteURL = record["WebsiteURL"].ToString();
+
+                    restaurantList.Add(restaurant);
+                }
+            }
+            else
+            {
+                DBConnect dBConnect = new DBConnect();
+
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "TP_GetAllRestaurants";
+    
+                DataSet ds = dBConnect.GetDataSetUsingCmdObj(cmd);
+
+                foreach (DataRow record in ds.Tables[0].Rows)
+                {
+                    Restaurant restaurant = new Restaurant();
+
+                    restaurant.RestaurantID = Convert.ToInt32(record["RestaurantID"]);
+                    restaurant.OwnerID = Convert.ToInt32(record["OwnerID"]);
+                    restaurant.Name = record["Name"].ToString();
+                    restaurant.Cuisine = record["Cuisine"].ToString();
+                    restaurant.StreetAddress = record["StreetAddress"].ToString();
+                    restaurant.City = record["City"].ToString();
+                    restaurant.State = record["State"].ToString();
+                    restaurant.ZipCode = Convert.ToInt32(record["ZipCode"]);
+                    restaurant.HoursOfOperation = record["HoursOfOperation"].ToString();
+                    restaurant.Email = record["Email"].ToString();
+                    restaurant.PhoneNum = record["PhoneNumber"].ToString();
+                    restaurant.Description = record["Description"].ToString();
+                    restaurant.OverallRating = Convert.ToDouble(record["OverallRating"]);
+                    restaurant.AvgFoodRating = Convert.ToDouble(record["AvgFoodRating"]);
+                    restaurant.AvgServiceRating = Convert.ToDouble(record["AvgServiceRating"]);
+                    restaurant.AvgAtmosphereRating = Convert.ToDouble(record["AvgAtmosphereRating"]);
+                    restaurant.AvgPriceRating = Convert.ToDouble(record["AvgPriceRating"]);
+                    restaurant.WebsiteURL = record["WebsiteURL"].ToString();
+
+                    restaurantList.Add(restaurant);
+                }
+            }
+            ViewBag.Cuisines = GetAllCuisines();
+            return View("SearchRestaurants", restaurantList);
+        }
+
+        private List<string> GetAllCuisines()
+        {
+            List<string> cuisines = new List<string>();
+
+            DBConnect dbConnect = new DBConnect();
 
             SqlCommand cmd = new SqlCommand();
             cmd.CommandType = CommandType.StoredProcedure;
-            cmd.CommandText = "TP_GetAllRestaurants";
+            cmd.CommandText = "TP_GetAllCuisines";
 
-            DataSet ds = dBConnect.GetDataSetUsingCmdObj(cmd);
+            DataSet ds = dbConnect.GetDataSetUsingCmdObj(cmd);
 
             foreach (DataRow record in ds.Tables[0].Rows)
             {
-                Restaurant restaurant = new Restaurant();
-
-                restaurant.RestaurantID = Convert.ToInt32(record["RestaurantID"]);
-                restaurant.OwnerID = Convert.ToInt32(record["OwnerID"]);
-                restaurant.Name = record["Name"].ToString();
-                restaurant.Cuisine = record["Cuisine"].ToString();
-                restaurant.StreetAddress = record["StreetAddress"].ToString();
-                restaurant.City = record["City"].ToString();
-                restaurant.State = record["State"].ToString();
-                restaurant.ZipCode = Convert.ToInt32(record["ZipCode"]);
-                restaurant.HoursOfOperation = record["HoursOfOperation"].ToString();
-                restaurant.Email = record["Email"].ToString();
-                restaurant.PhoneNum = record["PhoneNumber"].ToString();
-                restaurant.Description = record["Description"].ToString();
-                restaurant.OverallRating = Convert.ToDouble(record["OverallRating"]);
-                restaurant.AvgFoodRating = Convert.ToDouble(record["AvgFoodRating"]);
-                restaurant.AvgServiceRating = Convert.ToDouble(record["AvgServiceRating"]);
-                restaurant.AvgAtmosphereRating = Convert.ToDouble(record["AvgAtmosphereRating"]);
-                restaurant.AvgPriceRating = Convert.ToDouble(record["AvgPriceRating"]);
-                restaurant.WebsiteURL = record["WebsiteURL"].ToString();
-
-                restaurantList.Add(restaurant);
+                cuisines.Add(record["Cuisine"].ToString());
             }
-            return View("SearchRestaurants", restaurantList);
+            return cuisines;
         }
     }
 }
