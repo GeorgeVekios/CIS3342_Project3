88594b16575aaa822f324bb24e163d7b5b1877ff
tuo77176 <tuo77176@temple.edu>
2025-04-08 13:51:20 -0700

    Added References to Object Class Library. and pasted code from Controller to RestaurantDBOps

--- RestaurantDBOperations/GetRestaurantByIDOp.cs
+++ RestaurantDBOperations/GetRestaurantByIDOp.cs
@@ -1,13 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data.SqlClient;
+using System.Data;
+using System;
+using Utilities;
+using ObjectClassLibrary;
 
 namespace RestaurantDBOperations
 {
     public class GetRestaurantByIDOp
     {
-        private 
+        private DBConnect dbConnect = new DBConnect();
+
+        public Restaurant GetRestaurantByID()
+        {
+            Restaurant restaurant = new Restaurant();
+            DBConnect dBConnect = new DBConnect();
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "TP_GetRestaurantByID";
+
+            cmd.Parameters.AddWithValue("@RestaurantID", id);
+
+            DataSet ds = dBConnect.GetDataSetUsingCmdObj(cmd);
+
+            if (ds.Tables[0].Rows.Count > 0)
+            {
+                DataRow record = ds.Tables[0].Rows[0];
+
+                restaurant.RestaurantID = Convert.ToInt32(record["RestaurantID"]);
+                restaurant.OwnerID = Convert.ToInt32(record["OwnerID"]);
+                restaurant.Name = record["Name"].ToString();
+                restaurant.Cuisine = record["Cuisine"].ToString();
+                restaurant.StreetAddress = record["StreetAddress"].ToString();
+                restaurant.City = record["City"].ToString();
+                restaurant.State = record["State"].ToString();
+                restaurant.ZipCode = Convert.ToInt32(record["ZipCode"]);
+                restaurant.HoursOfOperation = record["HoursOfOperation"].ToString();
+                restaurant.Email = record["Email"].ToString();
+                restaurant.PhoneNum = record["PhoneNumber"].ToString();
+                restaurant.Description = record["Description"].ToString();
+                restaurant.OverallRating = Convert.ToDouble(record["OverallRating"]);
+                restaurant.AvgFoodRating = Convert.ToDouble(record["AvgFoodRating"]);
+                restaurant.AvgServiceRating = Convert.ToDouble(record["AvgServiceRating"]);
+                restaurant.AvgAtmosphereRating = Convert.ToDouble(record["AvgAtmosphereRating"]);
+                restaurant.AvgPriceRating = Convert.ToDouble(record["AvgPriceRating"]);
+                restaurant.WebsiteURL = record["WebsiteURL"].ToString();
+            }
+        }
     }
 }
