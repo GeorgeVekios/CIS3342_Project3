70112b669fda15cff919cc731a489839bd97bbf4
tuo77176 <tuo77176@temple.edu>
2025-04-09 00:21:32 -0700

    Create Review Page Fully Implemented

--- TP_RestaurantReviewApp/Controllers/ReviewController.cs
+++ TP_RestaurantReviewApp/Controllers/ReviewController.cs
@@ -1,13 +1,67 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ObjectClassLibrary;
+using RestaurantDBOperations;
+using ReviewDBOperations;
+using TP_RestaurantReviewApp.Models;
 
 namespace TP_RestaurantReviewApp.Controllers
 {
     public class ReviewController : Controller
     {
-        [HttpPost()]
+        [HttpGet()]
         public IActionResult CreateReview()
         {
-            return View();
+            GetAllRestaurantsOp getAllRestaurantsOp = new GetAllRestaurantsOp();
+            List<Restaurant> restaurants = getAllRestaurantsOp.GetAllRestaurants();
+
+            List<SelectListItem> restaurantList = new List<SelectListItem>();
+
+            foreach (Restaurant restaurant in restaurants)
+            {
+                SelectListItem item = new SelectListItem();
+                item.Value = restaurant.RestaurantID.ToString();
+                item.Text = restaurant.Name;
+                restaurantList.Add(item);
+            }
+
+            ViewBag.RestaurantList = restaurantList;
+
+            CreateReviewViewModel model = new CreateReviewViewModel
+            {
+                Review = new Review { CreatedAt = DateTime.Now }
+            };
+
+            return View(model);
+        }
+
+        [HttpPost()]
+        public IActionResult CreateReview(Review review)
+        {
+            GetAllRestaurantsOp getAllRestaurantsOp = new GetAllRestaurantsOp();
+            List<Restaurant> restaurants = getAllRestaurantsOp.GetAllRestaurants();
+
+            List<SelectListItem> restaurantList = new List<SelectListItem>();
+
+            foreach (Restaurant restaurant in restaurants)
+            {
+                SelectListItem item = new SelectListItem();
+                item.Value = restaurant.RestaurantID.ToString();
+                item.Text = restaurant.Name;
+                restaurantList.Add(item);
+            }
+
+            ViewBag.RestaurantList = restaurantList;
+
+            CreateReviewViewModel model = new CreateReviewViewModel
+            {
+                Review = new Review { CreatedAt = DateTime.Now }
+            };
+
+            AddReviewOp addReviewOp = new AddReviewOp();
+            addReviewOp.AddReview(review);
+
+            return View(model);
         }
     }
 }
---
+++ TP_RestaurantReviewApp/Models/CreateReviewViewModel.cs
@@ -0,0 +1,25 @@
+﻿using ObjectClassLibrary;
+
+namespace TP_RestaurantReviewApp.Models
+{
+    public class CreateReviewViewModel
+    {
+        private Review review;
+
+        public Review Review
+        {
+            get { return review; }
+            set { review = value; }
+        }
+
+        public CreateReviewViewModel ()
+        {
+
+        }
+
+        public CreateReviewViewModel(Review review)
+        {
+            this.review = review;
+        }
+    }
+}
