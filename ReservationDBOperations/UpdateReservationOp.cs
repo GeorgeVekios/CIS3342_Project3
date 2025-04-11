991cc1efeaf60c06c9cb601fd2ca39c4f7a4888d
georgev <georgevekios@gmail.com>
2025-04-11 15:56:59 -0400

    Added full UpdateReservation functionality at all MVC components

--- ReservationDBOperations/UpdateReservationOp.cs
+++ ReservationDBOperations/UpdateReservationOp.cs
@@ -5,28 +5,32 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ObjectClassLibrary;
+using ObjectClassLibrary
+
+
 using Utilities;
 
 namespace ReservationDBOperations
 {
-    public class AddReservationOp
+    public class UpdateReservationOp
     {
-        public int AddReservation(Reservation res)
+        public int UpdateReservation(Reservation res)
         {
             DBConnect dBConnect = new DBConnect();
             SqlCommand cmd = new SqlCommand();
             cmd.CommandType = CommandType.StoredProcedure;
-            cmd.CommandText = "TP_AddReservation"; 
+            cmd.CommandText = "TP_UpdateReservation";
 
             //map properties to stored procedure params
             cmd.Parameters.AddWithValue("@RestaurantID", res.RestId);
+            cmd.Parameters.AddWithValue("@ReservationID", res.ReservationId);
             cmd.Parameters.AddWithValue("@Name", res.Name);
             cmd.Parameters.AddWithValue("@PhoneNumber", res.PhoneNumber);
             cmd.Parameters.AddWithValue("@Email", res.Email);
             cmd.Parameters.AddWithValue("@DateTime", res.ResDT);
             cmd.Parameters.AddWithValue("@PartySize", res.PartySize);
             cmd.Parameters.AddWithValue("@Comments", res.Comments ?? (object)DBNull.Value); //null comments
+
 
             int rowsAffected = dBConnect.DoUpdateUsingCmdObj(cmd);
             return rowsAffected;
---
+++ TP_RestaurantReviewApp/Models/ManageReservationPageViewModel.cs
@@ -0,0 +1,11 @@
+﻿using ObjectClassLibrary;
+
+namespace TP_RestaurantReviewApp.Models
+{
+    public class ManageReservationPageViewModel
+    {
+        public int RestaurantID {  get; set; }
+        public Reservation Reservation { get; set; }
+        public string Message { get; set; }
+    }
+}
