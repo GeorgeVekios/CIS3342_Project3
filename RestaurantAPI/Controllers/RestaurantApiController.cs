5d0de01a24b543066f53abc0158c64045933415f
georgev <georgevekios@gmail.com>
2025-04-15 15:08:42 -0400

    Started API Implementation, added helper method to get Restaurant ID from User ID

---
+++ RestaurantAPI/Controllers/RestaurantApiController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ObjectClassLibrary;
+using RestaurantDBOperations;
+
+namespace RestaurantAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class RestaurantApiController : Controller
+    {
+        private static List<Restaurant> restaurants = new List<Restaurant>();
+        private static int _nextId = 1;
+
+        // GET: api/restaurant
+        [HttpGet]
+        public ActionResult<IEnumerable<Restaurant>> GetRestaurants()
+        {
+            return Ok(restaurants);
+        }
+
+        // GET: api/restaurant/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Restaurant> GetRestaurantByUserID(int userID)
+        {
+
+            var restaurant = restaurants.FirstOrDefault(r => r.RestaurantID == id);
+            if (restaurant == null)
+            {
+                return NotFound();
+            }
+            return Ok(restaurant);
+        }
+    }
+}
---
+++ RestaurantDBOperations/GetRestaurantByUserIDOp.cs
@@ -0,0 +1,39 @@
+﻿using ObjectClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utilities;
+
+namespace RestaurantDBOperations
+{
+    public class GetRestaurantByUserIDOp
+    {
+        private DBConnect dbConnect = new DBConnect();
+        public int GetRestaurantIDByUserID(int userID)
+        {
+
+            Restaurant restaurant = new Restaurant();
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "TP_GetRestaurantIDByUserID"; //TODO: Create stored procedure
+
+            cmd.Parameters.AddWithValue("@RestaurantID", userID);
+
+            DataSet ds = dbConnect.GetDataSetUsingCmdObj(cmd);
+            int restaurantID = 0;
+
+            if (ds.Tables[0].Rows.Count > 0)
+            {
+                DataRow record = ds.Tables[0].Rows[0];
+
+                restaurantID = Convert.ToInt32(record["RestaurantID"]);
+            }
+            return restaurantID;
+        }
+    }
+}
